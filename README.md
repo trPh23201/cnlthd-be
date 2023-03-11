21b5f7ebab0a7651420b00d4063b406f890b6392
TriPham <86234067+trPh23201@users.noreply.github.com>
2023-03-11 15:23:39 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Estibew-backend
+Example env:
+NODE_ENV=development
+SERVER_PORT=5000
+DATABASE_NAME=Shop_game
+DATABASE_PASSWORD=
+DATABASE_USER=postgres
+PTPMNM
---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+require('dotenv').config()
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
